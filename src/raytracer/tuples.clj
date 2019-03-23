2afe255cc1e998e07e5fbeca3db2b5eb3ac81f59
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2019-03-24 00:52:11 +0100

    Vector/point operations crudely implemented

--- src/raytracer/tuples.clj
+++ src/raytracer/tuples.clj
@@ -26,8 +26,44 @@
 (defn add [a b]
   (vec (map + a b)))
 
-;;; TODO/FIXME this is fishy
-;;; errors could accumulate and the result could be neither a vector or a point.
-;;; Should I keep track of the vector/point status somehow?
 (defn sub [a b]
   (vec (map - a b)))
+
+(defn neg [[x y z w]]
+  (vector (- x) (- y) (- z) (- w)))
+
+(defn mul [[x y z w] s]
+  (vector (* x s)
+          (* y s)
+          (* z s)
+          (* w s)))
+
+(defn div [[x y z w] s]
+  (vector (/ x s)
+          (/ y s)
+          (/ z s)
+          (/ w s)))
+
+(defn mag [[x y z w]]
+  (Math/sqrt (float (+ (* x x)
+                       (* y y)
+                       (* z z)
+                       (* w w)))))
+
+(defn norm [[x y z w :as v]]
+  (let [m (mag v)]
+    (vector (/ x m)
+            (/ y m)
+            (/ z m)
+            (/ w m))))
+
+(defn dot [[x1 y1 z1 w1] [x2 y2 z2 w2]]
+  (+ (* x1 x2)
+     (* y1 y2)
+     (* z1 z2)
+     (* w1 w2)))
+
+(defn cross [[x1 y1 z1 _] [x2 y2 z2 _]]
+  (svector (- (* y1 z2) (* z1 y2))
+           (- (* z1 x2) (* x1 z2))
+           (- (* x1 y2) (* y1 x2))))
--- test/raytracer/tuples_test.clj
+++ test/raytracer/tuples_test.clj
@@ -2,6 +2,8 @@
   (:require [clojure.test :refer :all]
             [raytracer.tuples :refer :all]))
 
+(def inv-sqrt-3 (/ (Math/sqrt 3)))
+
 (deftest test-is-about?
   (testing "general cases"
     (is (is-about? 1 1))
@@ -70,4 +72,60 @@
   (testing "subtracting two vectors"
     (is (eq (svector -2 -4 -6)
             (sub (svector 3 2 1)
-                 (svector 5 6 7))))))
+                 (svector 5 6 7)))))
+    (testing "subtracting a vector from the zero vector"
+      (is (eq (svector -1 2 -3)
+              (sub (svector 0 0 0) (svector 1 -2 3))))))
+
+(deftest test-neg
+  (testing "negating a tuple"
+    (is (eq [-1 2 -3 4]
+            (neg (vector 1 -2 3 -4))))))
+
+(deftest test-mul
+  (testing "multiplying a tuple by a scalar"
+    (eq [3.5 -7 10.5 -14] (mul [1 -2 3 -4] 3.5)))
+  (testing "multiplying a tuple by a fraction"
+    (eq [0.5 -1 1.5 -2] (mul [1 -2 3 -4] 0.5))))
+
+(deftest test-div
+  (testing "dividing a tuple by a scalar"
+    (eq [0.5 -1 1.5 -2]
+        (div [1 -2 3 -4] 2))))
+
+(defmacro do-test-mag [expected v]
+  `(testing ~(str "computing the magnitude of " v)
+     (is (is-about ~expected (mag ~(apply svector v))))))
+
+(deftest test-mag
+  (do-test-mag 1 [1 0 0])
+  (do-test-mag 1 [0 1 0])
+  (do-test-mag 1 [0 0 1])
+  (do-test-mag (Math/sqrt 14) [1 2 3])
+  (do-test-mag (Math/sqrt 14) [-1 -2 -3]))
+
+(defmacro do-test-norm [expected v]
+  `(testing ~(str "normalizing " v)
+     (let [res# (norm (apply svector ~v))]
+       (is (eq (apply svector ~expected) res#))
+       (is (is-about? 1 (mag res#))))))
+
+(deftest test-norm
+  (do-test-norm [1 0 0] [4 0 0])
+  (do-test-norm [inv-sqrt-3 inv-sqrt-3 inv-sqrt-3] [4 4 4])
+  (do-test-norm [(/ (Math/sqrt 14)) (/ 2 (Math/sqrt 14)) (/ 3 (Math/sqrt 14))]
+                [1 2 3]))
+
+(deftest test-dot
+  (is (is-about? 20
+                 (dot (svector 1 2 3)
+                      (svector 2 3 4)))))
+
+(deftest test-cross
+  (is (eq (svector -1 2 -1)
+          (cross (svector 1 2 3) (svector 2 3 4))))
+  (is (eq (svector 1 -2 1)
+          (cross (svector 2 3 4) (svector 1 2 3))))
+  (is (is-about? 0 (dot (svector -34 54 124)
+                        (cross (svector -34 54 124) (svector 34 12 1))))))
+
