4a54d3c037ba5025f5ad71a362a7edf45ac4a865
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2019-04-06 15:09:39 +0200

    Crazy-ass slow projection implementation
    Some features still missing, like colors for different objects

--- test/demo/simple_projection.clj
+++ test/demo/simple_projection.clj
@@ -2,21 +2,23 @@
   (:require [raytracer.svector :as svector]
             [raytracer.tuple :as tuple]
             [raytracer.point :as point]
+            [raytracer.color :as color]
             [raytracer.matrix :as matrix]
             [raytracer.canvas :as canvas]
             [raytracer.transform :as transform]
             [raytracer.ray :as ray]))
 
-(def canvas-size {:width 320, :height 200})
-(def canvas-scale [0.1 0.1])
+(def canvas-size {:width 32, :height 20})
+(def canvas-scale [1 1])
 
 (defn- create-simple-scene
   "Naive setup: the screen position in the scene decides the FOV and VFOV
 
   The screen orientation is always parallel to XY"
   []
-  {:camera (point/point -10 0 0)
-   :objects [(ray/sphere)]
+  {:camera (point/point 0 0 -10)
+   :objects [{:object (ray/change-transform (ray/sphere) (transform/scale 7 7 7))
+              :color [1 0 0]}]
    :screen-z 0})
 
 (defn- adjust-canvas [[x y]]
@@ -33,23 +35,33 @@
           (vector scene-z 1)))
 
 (defn- seq-ray [scene]
-  (let [pixel-to-coord-f (partial compute-pixel-coordinates (:scene-z scene))]
-    (for [i (range (:width canvas-size))
-          j (range (:height canvas-size))]
-      {:pixel [i j]
-       :ray (ray/create (:camera scene)
-                        (svector/norm (tuple/neg (pixel-to-coord-f scene)
-                                                 (:camera scene))))})))
+  (let [pixel-to-coord-f (partial compute-pixel-coordinates (:screen-z scene))]
+    (for [j (range (:height canvas-size))
+          i (range (:width canvas-size))]
+      (let [pixel [i j]]
+        {:pixel pixel
+         :ray (ray/create (:camera scene)
+                          (tuple/sub (pixel-to-coord-f pixel)
+                                     (:camera scene)))}))))
+
 
 
 (defn- compute-pixel [scene canvas {pixel :pixel, ray :ray}]
-  
-  )
+  ;;; COLOR missing! TODO/FIXME
+  (let [t-list (mapcat (fn [{obj :object, color :color}]
+                         (map :t (:values (ray/intersect ray obj))))
+                       (:objects scene))]
+    (if (not (empty? t-list))
+      (canvas/write canvas
+                    (first pixel) (second pixel)
+                    [255 0 0])
+      canvas)))
 
 (defn render-scene [scene]
-  (reduce (partial compute-pixel scene)
-          (canvas/create-canvas (:width canvas-size)
-                                (:height canvas-size))
-          (seq-ray scene)))
+  (spit "output.ppm"
+        (canvas/canvas-to-ppm (reduce (partial compute-pixel scene)
+                                      (canvas/create-canvas (:width canvas-size)
+                                                            (:height canvas-size))
+                                      (seq-ray scene)))))
 
 
