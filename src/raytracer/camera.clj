fbc3117578ee6458f8279bf5b71d224c5d9b4f1d
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2019-04-16 14:35:43 +0200

    camera/set-transform GREEN

--- src/raytracer/camera.clj
+++ src/raytracer/camera.clj
@@ -1,11 +1,14 @@
-(ns raytracer.camera)
+(ns raytracer.camera
+  (:require [raytracer.matrix :as matrix]))
 
 (defn camera [h-size v-size field-of-view]
   {:h-size h-size
    :v-size v-size
-   :fov field-of-view})
+   :fov field-of-view
+   :transform matrix/identity-matrix})
 
-(defn set-transform [transform-matrix])
+(defn set-transform [camera transform-matrix]
+  (assoc camera :transform transform-matrix))
 
 (defn ray-for-pixel [camera x y])
 
