12d1b278cce198f7e5591292f6a17a5bceba26ac
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2019-04-25 12:08:10 +0200

    get-cell GREEN

--- src/raytracer/perlin.clj
+++ src/raytracer/perlin.clj
@@ -17,6 +17,22 @@
   (doto (empty-grid rows columns)
     (fill-grid rows columns)))
 
+(defn- translate-to-positive [p scale]
+  (if (< p 0)
+    (- p (* scale (dec (int (/ p scale)))))
+    p))
+
+(defn- scale-positive-coord [p scale]
+  (mod (int (* p scale)) scale))
+
+(defn- scale-coord [p scale]
+  (scale-positive-coord (translate-to-positive p scale)
+                        scale))
+
+(defn- get-cell-scale [x-scale y-scale x y]
+  (vector (scale-coord y y-scale) 
+          (scale-coord x x-scale)))
+
 (defn get-cell [perlin-data [x y]]
-  (vector (int (* y (:y-scale perlin-data)))
-          (int (* x (:x-scale perlin-data)))))
+  (get-cell-scale (:x-scale perlin-data) (:y-scale perlin-data)
+                  x y))
