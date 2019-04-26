39d46ddad1d10f4995716232c048844ca095f0e5
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2019-04-26 23:27:55 +0200

    Shameful copy and paste of the perlin wikipedia algorithm
    Not as good looking as I expected?

--- test/demo/perlin_demo.clj
+++ test/demo/perlin_demo.clj
@@ -1,9 +1,9 @@
 (ns demo.perlin-demo
-  (:require [raytracer.perlin :as perlin]
+  (:require [raytracer.wikiperlin :as perlin]
             [raytracer.canvas :as canvas]))
 
-(def grid-width 4)
-(def grid-height 4)
+(def grid-width 100)
+(def grid-height 100)
 
 (defn write-color-to-canvas [width height colors]
   (reduce (fn color-to-canvas [canvas [color [x y]]]
@@ -14,19 +14,19 @@
 (defn convert-noise-to-color [noise]
   (map
    (fn noise-to-color [[x y noise]]
-                      (let [value (* 0.5 (- 1 noise))]
+                      (let [value (- 1 noise)]
                         (vector [value value value] (vector x y))))
    noise))
 
 (defn create-noise [perlin-data width height]
   (pmap (fn pixel-to-noise [[x y]]
-         (vector x y (perlin/noise perlin-data [(/ x width) (/ y height)])))
+         (vector x y (perlin/perlin perlin-data (/ x 10) (/ y 10))))
        (for [i (range height)
              j (range width)]
          (vector j i))))
 
 (defn render-demo
-  ([] (render-demo 200 200))
+  ([] (render-demo 400 400))
   ([width height]
    (let [perlin-data (perlin/create-perlin-data grid-width grid-height)]
      (spit "noise.ppm" (canvas/canvas-to-ppm
