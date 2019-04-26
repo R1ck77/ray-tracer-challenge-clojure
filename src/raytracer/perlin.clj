659eccd9e1b43ed22c0f3809ba16c5a6050e677f
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2019-04-26 16:09:45 +0200

    Remove old perlin functions/tests GREEN

--- src/raytracer/perlin.clj
+++ src/raytracer/perlin.clj
@@ -2,6 +2,8 @@
 (ns raytracer.perlin
   (:require [raytracer.svector :as svector]))
 
+(def neighbors [[0 0] [1 0] [0 1] [1 1]])
+
 (defn- empty-grid [width height]
   (into-array (take height (repeatedly #(make-array java.util.List width)))))
 
@@ -22,90 +24,15 @@
    :y-scale y-scale
    :grid (create-grid x-scale y-scale)})
 
-(defn get-cell [grid x y]
-  (aget grid y x))
-
-(defn- scale-coord [p scale]
-  (mod (int p) scale))
-
-(defn get-cell-corner [perlin-data [scaled-x scaled-y]]
-  (vector (scale-coord scaled-x (:x-scale perlin-data)) 
-          (scale-coord scaled-y (:y-scale perlin-data))))
-
-(defn make-positive [scaled-p scale]
-  (if (>= scaled-p 0)
-    scaled-p
-    (- scaled-p (* scale (dec (int (/ scaled-p scale)))))))
-
-(defn- scale-coordinate [p scale]
-  (make-positive (* p scale) scale))
-
 (defn scale-point
   "Scale the point so that it fits inside the proper grid after periodic boundary translation"
   [perlin-data [x y]]
-  (vector (scale-coordinate x (:x-scale perlin-data))
-          (scale-coordinate y (:y-scale perlin-data))))
-
-(def neighbors-delta [[0 0] [1 0] [0 1] [1 1]])
-
-(defn- combine
-  [x-scale y-scale [base-x base-y] [dx dy]]
-  (vector (mod (+ base-x dx) x-scale)
-          (mod (+ base-y dy) y-scale)))
-
-(defn get-neighbors
-  "Return the list of nodes"
-  [perlin-data scaled-point]  
-  (let [{:keys [x-scale y-scale]} perlin-data
-        base (get-cell-corner perlin-data scaled-point)]
-    (vec (map (partial combine x-scale y-scale base) neighbors-delta))))
-
-(defn compute-distances
-  "Compute the distance of a scaled point from the node vertices
-
-  Use svector because… It's there."
-  [neighbors [sx sy]]
-  (vec (map (fn single-distance [[gx gy :as neighbor]]
-              {:coords neighbor
-               :distance (svector/svector (- gx sx) (- gy sy) 0)})
-            neighbors)))
-
-(defn- dot-product
-  [grid
-   {[x y :as coords] :coords
-    distance :distance}]
-  {:coords coords
-   :dot (svector/dot distance (get-cell grid x y))})
-
-(defn compute-products
-  "Give a grid of gradients and a set of distances, compute a set of dot products"
-  [grid distances]
-  (vec (map (partial dot-product grid)
-            distances)))
+  (vector (* x (:x-scale perlin-data))
+          (* y (:y-scale perlin-data))))
 
 (defn- lerp [a0 a1 w]
   (+ (* (- 1 w) a0))
   (* w a1))
-
-(defn interpolate
-  "Find the final value for the point"
-  [products [x y]]
-  (let [x1 (nth products 0)
-        x2 (nth products 1)
-        x3 (nth products 2)
-        x4 (nth products 3)]
-    (lerp (lerp (:dot x1) (:dot x2) (- (first (:coords x1)) y))
-          (lerp (:dot x3) (:dot x4) (- (first (:coords x3)) y))
-          (- (second (:coords x1)) x))))
-
-(defn noise [perlin-data point]
-  (let [scaled-point (scale-point perlin-data point)]
-    (interpolate (compute-products (:grid perlin-data)
-                                   (compute-distances (get-neighbors perlin-data scaled-point)
-                                                      scaled-point))
-                 scaled-point)))
-
-(def neighbors [[0 0] [1 0] [0 1] [1 1]])
 
 (defn- compute-corners [x y]
   (vec
--- test/raytracer/perlin_test.clj
+++ test/raytracer/perlin_test.clj
@@ -32,15 +32,6 @@
       (is (= 20 (count (:grid perlin-data))))
       (is (= 30 (count (aget (:grid perlin-data) 0)))))))
 
-(deftest test-get-cell-corner
-  (let [perlin-data (perlin/create-perlin-data 5 3)]
-    (testing "get-cell-corner returns the correct indices for a scaled point inside the grid"
-      (is (= [0 0] (perlin/get-cell-corner perlin-data [0.1 0.1])))
-      (is (= [4 2] (perlin/get-cell-corner perlin-data [4.1 2.1])))
-      (is (= [2 1] (perlin/get-cell-corner perlin-data [2.1 1.1]))))
-    (testing "get-cell-corner returns the correct indices for a scaled point outside the grid"
-      (is (= [3 1] (perlin/get-cell-corner perlin-data [8.1 7.1]))))))
-
 (deftest test-get-scaled-point-bounds
   (testing "return the correct indices for a scaled point inside the grid"
     (is (= {:corners [[0 0] [1 0] [0 1] [1 1]]
@@ -90,67 +81,8 @@
       (is (v= [2.5 1.5]
               (perlin/scale-point perlin-data [0.5 0.5]))))
     (testing "negative point"
-      (is (v= [(- 5 4.5) (- 3 2.7)]
+      (is (v= [-4.5 -2.7]
               (perlin/scale-point perlin-data [-0.9 -0.9])))
-      (is (v= [(- 20 19.5) (- 9 8.7)]
+      (is (v= [-19.5 -8.7]
               (perlin/scale-point perlin-data [-3.9 -2.9]))))))
 
-(deftest test-get-neighbors
-  (let [perlin-data (perlin/create-perlin-data 5 3)]
-    (testing "conditions for point in the higher left corner"
-      (is (= [[4 2] [0 2] [4 0] [0 0]]
-             (perlin/get-neighbors perlin-data [4.9 2.9]))))
-    (testing "condition for the point in the lower right corner"
-      (is (= [[0 0] [1 0] [0 1] [1 1]]
-             (perlin/get-neighbors perlin-data [0.5 0.5]))))))
-
-(deftest test-get-distances
-  (testing "given a point in the grid and a set of neighbors, compute the distances"
-    (is (= [{:coords [0 0]
-              :distance (svector/svector 0 0 0)}
-             {:coords [0 1]
-              :distance (svector/svector 0 1 0)}
-             {:coords [1 0]
-              :distance (svector/svector 1 0 0)}
-             {:coords [1 1]
-              :distance (svector/svector 1 1 0)}]
-           (perlin/compute-distances [[0 0] [0 1] [1 0] [1 1]]
-                                     [0 0])))))
-
-
-(defn- create-fake-perlin-data []
-  (let [grid (perlin/create-grid 5 3)]
-    (doseq [i (range 3), j (range 5)]
-      (aset grid i j (svector/svector j i 0)))
-    {:x-scale 5
-     :y-scale 3
-     :grid grid}))
-
-(deftest test-get-products
-  (let [perlin-data (create-fake-perlin-data)]
-    (testing "given a set of distances and the grid, compute the dot products"
-      (is (= [{:coords [0 0]
-               :dot 0}
-               {:coords [1 1]
-                :dot 2}              
-               {:coords [1 0]
-                :dot 1}
-               {:coords [0 1]
-                :dot 1}]             
-             (perlin/compute-products (:grid perlin-data) [{:coords [0 0]
-                                                             :distance (svector/svector 0 0 0)}
-                                                            {:coords [1 1]
-                                                             :distance (svector/svector 1 1 0)}
-                                                            {:coords [1 0]
-                                                             :distance (svector/svector 1 0 0)}
-                                                            {:coords [0 1]
-                                                             :distance (svector/svector 0 1 0)}]))))))
-
-(deftest test-noise
-  (let [perlin-data (perlin/create-perlin-data 7 13)]
-    (testing "noise returns some value for some special points"
-      (is (perlin/noise perlin-data [0 0]) 0)
-      (is (perlin/noise perlin-data [0.9999 0.9999]) 0))
-    (testing "noise returns some value for points outside the grid"
-      (is (perlin/noise perlin-data [-0.2 -0.4]) 0)
-      (is (perlin/noise perlin-data [1.3 4.5]) 0))))
