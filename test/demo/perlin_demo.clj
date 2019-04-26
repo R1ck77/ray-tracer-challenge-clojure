a8ca5ec6b1b67831136086f9bee8f1daf62f4680
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2019-04-26 10:07:19 +0200

    Minor changes to the perlin demo
    Split some methods and add parallelization

--- test/demo/perlin_demo.clj
+++ test/demo/perlin_demo.clj
@@ -2,24 +2,34 @@
   (:require [raytracer.perlin :as perlin]
             [raytracer.canvas :as canvas]))
 
-(def grid-width 320)
-(def grid-height 200)
+(def grid-width 32)
+(def grid-height 20)
+
+(defn write-color-to-canvas [width height colors]
+  (reduce (fn color-to-canvas [canvas [color [x y]]]
+            (canvas/write canvas x y color))
+          (canvas/create-canvas width height)
+          colors))
+
+(defn convert-noise-to-color [noise]
+  (map
+   (fn noise-to-color [[x y noise]]
+                      (let [value (- 1 noise)]
+                        (vector [value value value] (vector x y))))
+   noise))
+
+(defn create-noise [perlin-data width height]
+  (pmap (fn pixel-to-noise [[x y]]
+         (vector x y (perlin/noise perlin-data [(/ x width) (/ y height)])))
+       (for [i (range height)
+             j (range width)]
+         (vector j i))))
 
 (defn render-demo
   ([] (render-demo 320 200))
   ([width height]
    (let [perlin-data (perlin/create-perlin-data grid-width grid-height)]
-     (let [canvas (reduce (fn color-to-canvas [canvas [color [x y]]]
-                     (canvas/write canvas x y color))
-                   (canvas/create-canvas width height)
-                   (map
-                    (fn noise-to-color [[x y noise]]
-                      (let [value (- 1 noise)]
-                        (vector [value value value] (vector x y))))
-                    (map
-                     (fn pixel-to-noise [[x y]]
-                       (vector x y (perlin/noise perlin-data [(/ x width) (/ y height)])))
-                     (for [i (range height)
-                           j (range width)]
-                       (vector j i)))))]
-      (spit "noise.ppm" (canvas/canvas-to-ppm canvas))))))
+     (spit "noise.ppm" (canvas/canvas-to-ppm
+                        (write-color-to-canvas width height
+                                               (convert-noise-to-color           
+                                                (create-noise perlin-data width height))))))))
