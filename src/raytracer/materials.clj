89d96611fa16a2074d3fc6f07b71d520eaa27da9
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2019-05-10 20:55:38 +0200

    Convert the material to a record

--- src/raytracer/materials.clj
+++ src/raytracer/materials.clj
@@ -1,18 +1,24 @@
 (ns raytracer.materials
   (:require [raytracer.pattern :as pattern])) ;;;TODO/FIXME change the name to material
 
+(defrecord Material
+    [color
+     ambient diffuse specular shiness
+     reflectivity refractive-index
+     transparency])
+
 (defn material
   ([]
-   {:color [1 1 1]
-    :ambient 0.1
-    :diffuse 0.9
-    :specular 0.9
-    :shiness 200.0
-    :reflectivity 0.0
-    :transparency 0.0
-    :refractive-index 1.0
-    :pattern nil}) ;; TODO/FIXME very crude. generalize in a color getter function and remove color!
-                   ;; you can probably fit a texture, uv mapping or something here, and make color a special case!
+   (map->Material {:color [1 1 1]
+                   :ambient 0.1
+                   :diffuse 0.9
+                   :specular 0.9
+                   :shiness 200.0
+                   :reflectivity 0.0
+                   :transparency 0.0
+                   :refractive-index 1.0
+                   :pattern nil})) ;; TODO/FIXME very crude. generalize in a color getter function and remove color!
+                                   ;; you can probably fit a texture, uv mapping or something here, and make color a special case!
   ([& {:as args}]
    (merge (material) args)))
 
