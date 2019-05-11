4e9df4dc167c61e781463fdc7b0fafa30f4f983a
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2019-05-11 21:24:55 +0200

    Fix the clock demo

--- test/demo/clock.clj
+++ test/demo/clock.clj
@@ -1,9 +1,9 @@
 (ns demo.clock
-  (:require [raytracer.svector :refer :all]
-            [raytracer.point :refer :all]
-            [raytracer.canvas :refer :all]
-            [raytracer.matrix :refer :all]            
-            [raytracer.transform :refer :all]))
+  (:require [raytracer.svector :as svector]
+            [raytracer.point :as point]
+            [raytracer.canvas :as canvas]
+            [raytracer.matrix :as matrix]            
+            [raytracer.transform :as transform]))
 
 
 (def canvas-size [40 30])
@@ -11,14 +11,14 @@
 (def clock-radius 10)
 
 (defn clock-position [angle-rad]
-  ;;; TODO/FIXME some multiplications could be cached. Whatever
-  (transform (translate (/ (first canvas-size) 2)
-                        (/ (second canvas-size) 2)
-                        0
-                        (scale -1 -1 1
-                               (rotate-z angle-rad
-                                         (translate 0 clock-radius 0))))
-             (point 0 0 0)))
+;;; TODO/FIXME some multiplications could be cached. Whatever
+  (matrix/transform (transform/translate (/ (first canvas-size) 2)
+                                         (/ (second canvas-size) 2)
+                                         0
+                                         (transform/scale -1 -1 1
+                                                          (transform/rotate-z angle-rad
+                                                                              (transform/translate 0 clock-radius 0))))
+                    (point/point 0 0 0)))
 
 (defn clock-angles-seq []
   (let [slice (/ Math/PI 6)]
@@ -29,7 +29,9 @@
 
 (defn render-demo []
   (spit "sad_clock.ppm"
-        (canvas-to-ppm (reduce (fn [canvas [x y _ _]]
-                                 (write canvas (int x) (int y) [1 1 1]))
-                               (apply create-canvas canvas-size)          
-                               (compute-clock-positions)))))
+        (canvas/canvas-to-ppm (reduce (fn [canvas point]
+                                        (let [x (:x point)
+                                              y (:y point)]
+                                         (canvas/write canvas (int x) (int y) [1 1 1])))
+                                      (apply canvas/create-canvas canvas-size)          
+                                      (compute-clock-positions)))))
