dfc93b9ecd6c451f7ea46378300f6033e0b9698c
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2019-05-12 10:26:55 +0200

    Add more renderings to the coverage demo

--- test/demo/all_demos_coverage.clj
+++ test/demo/all_demos_coverage.clj
@@ -1,13 +1,15 @@
 (ns demo.all-demos-coverage
   (:require [clojure.test :refer :all]
+            [raytracer.world :as world]
             [demo.projectile :as projectile]
             [demo.clock :as clock]
             [demo.simple-projection :as simple-projection]
             [demo.phong-sphere :as phong-sphere]
             [demo.phong-scene :as phong-scene]
             [demo.plane-demo :as plane-demo]
+            [demo.fresnel-demo :as fresnel]
+            [demo.perlin-demo :as perlin]
             [demo.pattern-demo :as pattern-demo]
-            [raytracer.world :as world]
             [demo.reflection-demo :as reflections-demo]))
 
 
@@ -21,7 +23,7 @@
     (projectile/simulate-dragless-cannon 45 23))
   (run-demo "clock demo"
     (clock/render-demo))
-  (run-demo "simple projection demo"
+  (run-demo "projection demo"
     (simple-projection/render-demo))
   (run-demo "tiny phong sphere demo"
     (phong-sphere/render-demo 8 4))
@@ -33,7 +35,13 @@
             (pattern-demo/render-demo 10 5))
   (run-demo "tiny reflections demo"
             (with-redefs [world/*maximum-reflections* 2]
-              (reflections-demo/render-demo 10 5))))
+              (reflections-demo/render-demo 10 5)))
+  (run-demo "tiny fresnel demo"
+            (fresnel/render-demo 10 5))
+  (run-demo "tiny perlin demo"
+            (do
+              (perlin/render-demo 10 10)
+              (perlin/render-animation 10 10 3))))
 
 (defn -main [& args]
   (run-all-demos)
