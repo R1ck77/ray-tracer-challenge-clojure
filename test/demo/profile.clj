743a473be55e922f836da00058493279674753ed
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2019-05-24 17:06:55 +0200

    Change the profiler to profile the cube

--- test/demo/profile.clj
+++ test/demo/profile.clj
@@ -1,10 +1,10 @@
 (ns demo.profile
-  (:require [demo.reflection-demo :as demo]
+  (:require [demo.cubes-demo :as demo]
             [raytracer.world :as world]
             [raytracer.camera :as camera]))
 
 (defn -main [& args]
-  (with-redefs [demo/*image-resolution* [1920 1080]
+  (with-redefs [demo/*image-resolution* [(* 2 1920) (* 2 1080)]
                 camera/*map* map
-                world/*maximum-reflections* 8]
+                world/*maximum-reflections* 1]
     (time (demo/render-demo))))
