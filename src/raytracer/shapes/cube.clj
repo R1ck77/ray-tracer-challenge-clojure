689827771c2c88e00fac6d944ecab3f75d865134
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2019-05-25 16:53:51 +0200

    deftype-based cube intersection optimization

--- src/raytracer/shapes/cube.clj
+++ src/raytracer/shapes/cube.clj
@@ -10,28 +10,57 @@
 
 (defrecord Cube [material transform inverse-transform])
 
-(defn- axis-intersection [numerator direction]
+(defn- axis-intersection ^double [numerator direction]
   (if (> (Math/abs (float direction)) const/EPSILON)
     (/ numerator direction)
     (if (> numerator 0)
       Double/POSITIVE_INFINITY
       Double/NEGATIVE_INFINITY)))
 
-(defn- check-axis [origin direction]
-  (let [a (axis-intersection (- 0 1 origin) direction)
-        b (axis-intersection (- 1 origin) direction)]
-    (if (> a b) [b a] [a b])))
+(defprotocol TmpContent
+  (getOrigin [this])
+  (setValues [this new-origin new-direction])
+  (getDirection [this]))
+
+(deftype Tmp [^:unsynchronized-mutable origin
+              ^:unsynchronized-mutable direction]
+  TmpContent
+  (getOrigin [this]
+    (.origin this))
+  (setValues [this new-origin new-direction]
+    (set! origin new-origin)
+    (set! direction new-direction))
+  (getDirection [this]
+    (.direction this)))
+
+(defn- check-axis [^Tmp tmp ^doubles min-array ^doubles max-array ^long index]
+  (let [a (axis-intersection (- 0 1 (.getOrigin tmp)) (.getDirection tmp))
+        b (axis-intersection (- 1 (.getOrigin tmp)) (.getDirection tmp))]
+    (if (> a b)
+      (do
+        (aset min-array index b)
+        (aset max-array index a))
+      (do
+        (aset min-array index a)
+        (aset max-array index b)))))
 
 (defn- local-intersect [cube ray]
-  (let [[tminx tmaxx] (check-axis (:x (:origin ray)) (:x (:direction ray)))
-        [tminy tmaxy] (check-axis (:y (:origin ray)) (:y (:direction ray)))
-        [tminz tmaxz] (check-axis (:z (:origin ray)) (:z (:direction ray)))]
-    (let [t1 (max tminx tminy tminz)
-          t2 (min tmaxx tmaxy tmaxz)]
-      (if (> t2 t1) 
-        [(intersection/intersection t1 cube)
-         (intersection/intersection t2 cube)]
-        []))))
+  (let [direction (:direction ray)
+        origin (:origin ray)
+        min-array (double-array 3)
+        max-array (double-array 3)]
+    (let [tmp ^Tmp (->Tmp (:x origin) (:x direction))]
+      (check-axis tmp min-array max-array 0)
+      (.setValues tmp (:y origin) (:y direction))
+      (check-axis tmp min-array max-array 1)
+      (.setValues tmp (:z origin) (:z direction))
+      (check-axis tmp min-array max-array 2)
+      (let [t1 (Math/max (aget min-array 0) (Math/max (aget min-array 1) (aget min-array 2)))
+            t2 (Math/min (aget max-array 0) (Math/min (aget max-array 1) (aget max-array 2)))]
+        (if (> t2 t1) 
+          [(intersection/intersection t1 cube)
+           (intersection/intersection t2 cube)]
+          [])))))
 
 (defn- sign [value]
   (if (< value 0) -1 1))
