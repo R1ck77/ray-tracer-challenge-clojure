3fa423861144e5ffae7772d883f43b232cf0fd61
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2019-05-30 18:54:39 +0200

    Remove reflection from tuple_test GREEN

--- test/raytracer/tuple_test.clj
+++ test/raytracer/tuple_test.clj
@@ -8,11 +8,11 @@
 (deftest test-add
   (testing "adding two tuple"
     (is (t= (tuple/tuple 1 1 6 1)
-                     (.add (tuple/tuple 3 -2 5 1)
-                           (tuple/tuple -2 3 1 0))))
+            (tuple/add (tuple/tuple 3 -2 5 1)
+                       (tuple/tuple -2 3 1 0))))
     (is (t= (tuple/tuple 11 102 203 0)
-                     (.add (tuple/tuple 1 2 3 0)
-                           (tuple/tuple 10 100 200 0)))))
+            (tuple/add (tuple/tuple 1 2 3 0)
+                       (tuple/tuple 10 100 200 0)))))
   (testing "add more tuples"
     (is (t= (tuple/tuple 8 8 8 8)
             (tuple/add-all (vector (tuple/tuple 1 2 1 2)
@@ -23,33 +23,33 @@
 (deftest test-sub
   (testing "subtracting two points"
     (is (t= (svector/svector -2 -4 -6)
-            (.sub (point/point 3 2 1)
-                  (point/point 5 6 7)))))
+            (tuple/sub (point/point 3 2 1)
+                       (point/point 5 6 7)))))
   (testing "subtracting a vector from a point"
     (is (t= (point/point -2 -4 -6)
-            (.sub (point/point 3 2 1)
-                  (svector/svector 5 6 7)))))
+            (tuple/sub (point/point 3 2 1)
+                       (svector/svector 5 6 7)))))
   (testing "subtracting two vectors"
     (is (t= (svector/svector -2 -4 -6)
-            (.sub (svector/svector 3 2 1)
-                  (svector/svector 5 6 7)))))
+            (tuple/sub (svector/svector 3 2 1)
+                       (svector/svector 5 6 7)))))
   (testing "subtracting a vector from the zero vector"
     (is (t= (svector/svector -1 2 -3)
-            (.sub (svector/svector 0 0 0)
-                  (svector/svector 1 -2 3))))))
+            (tuple/sub (svector/svector 0 0 0)
+                       (svector/svector 1 -2 3))))))
 
 (deftest test-neg
   (testing "negating a tuple"
     (is (t= (tuple/tuple -1 2 -3 4)
-            (.neg (tuple/tuple 1 -2 3 -4))))))
+            (tuple/neg (tuple/tuple 1 -2 3 -4))))))
 
 (deftest test-mul
   (testing "dividing a tuple by a scalar"
     (is (t=  (tuple/tuple 1 -2 3 -4)
-             (.mul (tuple/tuple 0.5 -1 1.5 -2) 2)))))
+             (tuple/mul (tuple/tuple 0.5 -1 1.5 -2) 2)))))
 
 (deftest test-div
   (testing "dividing a tuple by a scalar"
     (is (t= (tuple/tuple 0.5 -1 1.5 -2)
-            (.div (tuple/tuple 1 -2 3 -4) 2)))))
+            (tuple/div (tuple/tuple 1 -2 3 -4) 2)))))
 
