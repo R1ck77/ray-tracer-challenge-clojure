0facc87a4eb30d463e335890b74e4f80db65bb4d
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2019-06-05 17:29:29 +0200

    Add all cone-related tests RED
    A lot of duplication with the code also, not clear what to do with it

---
+++ src/raytracer/shapes/cone.clj
@@ -0,0 +1,118 @@
+(ns raytracer.shapes.cone
+  (:require [raytracer.const :as const]
+            [raytracer.tuple :as tuple]
+            [raytracer.svector :as svector]
+            [raytracer.matrix :as matrix]
+            [raytracer.point :as point]
+            [raytracer.material :as material]
+            [raytracer.shapes.shared :as shared]
+            [raytracer.intersection :as intersection]))
+
+(defn- check-cap [ray t]
+  (let [point (tuple/add (:origin ray) (tuple/mul (:direction ray) t))
+        x (:x point)
+        z (:z point)]
+    (<= (+ (* x x)
+           (* z z))
+        1)))
+
+(defn- intersect-cap [cone ray]
+  (let [y-direction (-> ray :direction :y)]
+    (if (or (not (:closed cone))
+            (< (-> y-direction float Math/abs) const/EPSILON))
+      []
+      ;;; TODO/FIXME? slow, probably
+      (map #(intersection/intersection % cone)
+           (filter #(check-cap ray %)
+                   (map #(/ (- (get cone %)
+                               (-> ray :origin :y))
+                            y-direction)  [:minimum :maximum]))))))
+
+(defn is-within-bounds? [cone ray t]
+  (let [y-intersection (+ (* (:y (:direction ray)) t) (:y (:origin ray)))]
+    (and (> y-intersection (:minimum cone))
+         (< y-intersection (:maximum cone)))))
+
+(defn- create-intersections [cone ray [t1 t2 :as xt]]
+  (if xt
+    (let [partial (if (is-within-bounds? cone ray t1)
+                    [(intersection/intersection t1 cone)]
+                    [])]
+      (if (is-within-bounds? cone ray t2)
+        (conj partial (intersection/intersection t2 cone))
+        partial))
+    []))
+
+(defmacro map-to-vector [f & args]
+  `(vector ~@(map #(list f %) args)))
+
+(defn- t-for-side-intersections [ray-object-space]
+  (let [direction (:direction ray-object-space)
+        x-direction (:x direction)
+        z-direction (:z direction)
+        a (+ (* x-direction x-direction)
+             (* z-direction z-direction))]
+    (if (>= a const/EPSILON)
+      (let [origin (:origin ray-object-space)
+            x-origin (:x origin)
+            z-origin (:z origin)
+            b (+ (* 2 x-origin x-direction)
+                 (* 2 z-origin z-direction))
+            c (+ (* x-origin x-origin)
+                 (* z-origin z-origin)
+                 -1)
+            discriminant (- (* b b)
+                            (* 4 a c))]
+        (if (>= discriminant 0)
+          (let [√discriminant (Math/sqrt discriminant)]
+            (list (- (/ (+ √discriminant b)
+                        (* 2 a)))
+                  (/ (- √discriminant  b)
+                     (* 2 a)))))))))
+
+(defn- intersect-sides [cone ray-object-space]
+  (create-intersections cone
+                        ray-object-space
+                        (t-for-side-intersections ray-object-space)))
+
+(defn- local-intersect [cone ray-object-space]
+  (concat (intersect-sides cone ray-object-space)
+          (intersect-cap cone ray-object-space)))
+
+(defn- compute-cone-side-normal [point-object-space]
+    (svector/svector (:x point-object-space) 0 (:z point-object-space)))
+
+(defn- compute-cone-normal [this point-object-space]
+  (let [x (:x point-object-space)
+        y (:y point-object-space)
+        z (:z point-object-space)
+        dist (+ (* x x) (* z z))]
+    (cond 
+      (and (< dist 1) (>= y (- (:maximum this) const/EPSILON))) (svector/svector 0 1 0)
+      (and (< dist 1) (<= y (+ (:minimum this) const/EPSILON))) (svector/svector 0 -1 0)
+      :default (compute-cone-side-normal point-object-space))))
+
+(defrecord Cone [minimum maximum closed inverse-transform])
+
+(extend-type Cone
+  shared/Intersectable
+  (local-intersect [this ray-object-space]
+    (local-intersect this ray-object-space))
+  shared/Surface
+  (compute-normal [this point]
+    (tuple/normalize
+     (shared/as-vector
+      (matrix/transform (matrix/transpose (:inverse-transform this))
+                        (compute-cone-normal this
+                                                 (matrix/transform (:inverse-transform this) point)))))))
+
+(defn cone 
+  [& {:as args-map}]
+  (let [args (merge {:transform matrix/identity-matrix
+                     :minimum Double/NEGATIVE_INFINITY
+                     :maximum Double/POSITIVE_INFINITY}
+                    args-map)]
+    (->Cone (:minimum args)
+                (:maximum args)
+                (:closed args)
+                (matrix/invert (:transform args) 4))))
---
+++ test/raytracer/shapes/cone_test.clj
@@ -0,0 +1,87 @@
+(ns raytracer.shapes.cone-test
+  (:require [clojure.test :refer :all]
+            [raytracer.test-utils :refer :all]
+            [raytracer.ray :as ray]
+            [raytracer.tuple :as tuple]
+            [raytracer.point :as point]
+            [raytracer.svector :as svector]
+            [raytracer.transform :as transform]
+            [raytracer.shapes.cone :as cone]
+            [raytracer.shapes.shared :as shared]))
+
+(def a-cone (cone/cone))
+
+(def √2 (Math/sqrt 2))
+(def half√2 (/ √2 2))
+
+;;; TODO/FIXME fix this test by replacing arrays with ArrayList
+(comment
+  (deftest test-equality
+    (testing "Two cones are equals if the share the same characteristics"
+      (is (= (cone/cone)
+             (cone/cone)))
+      (let [transform (transform/translate 1 2 3 )]
+        (is (= (cone/cone transform) (cone/cone transform)))
+        (is (not= (cone/cone transform) (cone/cone)))))))
+
+(deftest test-constructor
+  (testing "The default minimum and maximum for a cone"
+    (is (= Double/POSITIVE_INFINITY (:maximum a-cone)))
+    (is (= Double/NEGATIVE_INFINITY (:minimum a-cone))))
+  (testing "A cone can be created with a set minimum"
+    (let [cone (cone/cone :minimum 12)]
+      (is (= Double/POSITIVE_INFINITY (:maximum cone)))
+      (is (= 12 (:minimum cone)))))
+  (testing "A cone can be created with a set maximum"
+    (let [cone (cone/cone :maximum 100)]
+      (is (= 100 (:maximum cone)))
+      (is (= Double/NEGATIVE_INFINITY (:minimum a-cone)))))
+  (testing "A cone can be created with a set minimum and maximum"
+    (let [cone (cone/cone :maximum 100 :minimum 12)]
+      (is (= 12 (:minimum cone)))
+      (is (= 100 (:maximum cone)))))
+  (testing "The default closed value for a cone"
+    (is (not (:closed a-cone))))
+  (testing "Filled cones can be created"
+    (is (:closed (cone/cone :closed true)))))
+
+(defmacro test-compute-cone-normal [point expected-normal]
+  `(is
+    (t= (tuple/normalize ~expected-normal)
+        (shared/compute-normal a-cone (apply point/point ~point)))))
+
+(defmacro test-intersecting-cone-with-end-caps [origin direction expected-count]
+  `(let [cone# (cone/cone :closed true
+                          :minimum -0.5
+                          :maximum 0.5)
+         ray# (ray/ray (apply point/point ~origin)
+                       (apply svector/svector ~direction))]
+     (is (= ~expected-count
+            (count (shared/local-intersect cone# ray#))))))
+
+(defmacro test-intersecting-cone-with-ray [origin direction expected-result]
+  `(let [cone# (cone/cone)
+         ray# (ray/ray (apply point/point ~origin)
+                       (apply svector/svector ~direction))]
+     (is (t= ~expected-result
+             (map :t (shared/local-intersect cone# ray#))))))
+
+(deftest test-local-intersect
+  (testing "Intersecting a cone with a ray"
+    (test-intersecting-cone-with-ray [0 0 -5] [0 0 1] [5 5])
+    (test-intersecting-cone-with-ray [0 0 -5] [1 1 1] [8.66025 8.66025])
+    (test-intersecting-cone-with-ray [1 1 -5] [-0.5 -1 1] [4.55006 49.44994]) )
+  (testing "Intersecting a cone with a ray parallel to one of its halves"
+    (is [0.35355]
+        (shared/local-intersect a-cone
+                                (ray/ray (point/point 0 0 -1)
+                                         (tuple/normalize (svector/svector 0 1 1))))))
+  (testing "Intersecting a cone's end caps"
+    (test-intersecting-cone-with-end-caps [0, 0, -5]    [0, 1, 0] 0)
+    (test-intersecting-cone-with-end-caps [0, 0, -0.25] [0, 1, 1] 2)
+    (test-intersecting-cone-with-end-caps [0, 0, -0.25] [0, 1, 0] 4))
+  (testing "Computing the normal vector on a cone"
+    (test-compute-cone-normal [0 0 0] [0 0 0])
+    (test-compute-cone-normal [1 1 1] [1 (- (Math/sqrt 2)) 1])
+    (test-compute-cone-normal [-1 -1 0] [-1 1 0])))
+
