a036291469d9c7f00df312442d2ef73be0d39ed3
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2019-06-06 11:44:37 +0200

    Create an optimization macro that sorts of mimic filter
    Should works for collections of size known at compile time

--- src/raytracer/utils.clj
+++ src/raytracer/utils.clj
@@ -8,65 +8,21 @@
   `(vector
     ~@(map #(list f %) v)))
 
-
-(defn template1 [f [a]]
-  (if (f a)
-    [a]
-    []))
-
-(defn template2 [f [a b]]
-  (if (f a)
-    (if (f b)
-      [a b]
-      [a])
-    (if (f b)
-      [b]
-      [])))
-
-
-(defn template3 [f [a b c]]
-  (if (f a)
-    (if (f b)
-      (if (f c)
-        [a b c]
-        [a b])
-      (if (f c)
-        [a c]
-        [a]))
-    (if (f b)
-      (if (f c)
-        [b c]
-        [b])
-      (if (f c)
-        [c]
-        []))))
-
-;;; [A] (A [a] [!a])
-;;; 
-;;; [A B] (A (B [a b] [a)
-;;;          (B [b] []))
-;;; 
-;;; [A B C] (A (B (C [a b c] [a b])
-;;;               (C [a c] [a]))
-;;;            (B (C [b c] [b])
-;;;               (C [c] [])))
-
-
-(defn templ
-  ([values]
-   (templ values []))
-  ([values carried]
+(defn- expand-comparison
+  ([f values carried]
    (let [next (first values)
          remaining (rest values)]
      (if (empty? remaining)
        ;;; last one
-       (vector (str next)
+       (list 'if (list f next)
                (conj carried next)
                carried)
        ;;; more to come
-       (vector (str next)
-               (templ remaining (conj carried next))
-               (templ remaining carried))
-       )
-     )))
+       (list 'if (list f next)
+               (templ f remaining (conj carried next))
+               (templ f remaining carried))))))
 
+(defmacro mfilter
+  "Unroll the filtering on a sequence of known elements to reproduce the filter behavior"
+  [f values]
+  (templ f values []))
