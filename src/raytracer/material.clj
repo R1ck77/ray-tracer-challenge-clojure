19a2da72f329e95052a9c429663d5b6ffa31385d
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2019-06-10 18:25:42 +0200

    WIP to uniform material's color and pattern color

--- src/raytracer/material.clj
+++ src/raytracer/material.clj
@@ -10,28 +10,23 @@
 
 (defn material
   ([]
-   (map->Material {:color (color/color 1 1 1)
-                   :ambient 0.1
+   (map->Material {:ambient 0.1
                    :diffuse 0.9
                    :specular 0.9
                    :shiness 200.0
                    :reflectivity 0.0
                    :transparency 0.0
                    :refractive-index 1.0
-                   :pattern nil})) ;; TODO/FIXME very crude. generalize in a color getter function and remove color!
-                                   ;; you can probably fit a texture, uv mapping or something here, and make color a special case!
+                   :pattern (pattern/solid (color/color 1 1 1))}))
   ([& {:as args}]
    (merge (material) args)))
 
-(def void-material (material :color (color/color 0 0 0)
-                             :transparency 1.0
-                             :refractive-index 1.0))
+(def void-material (material :transparency 1.0
+                             :refractive-index 1.0
+                             :pattern (pattern/solid (color/color 0 0 0))))
 
 (defn get-color [object point]
-  (let [material (:material object)]
-    (if-let [pattern (:pattern material)]
-      (pattern/color-at pattern object point)
-      (:color material))))
+  (pattern/color-at (-> object :material :pattern) object point))
 
 (defn update-material [material & {:as args}]
   (merge material args))
