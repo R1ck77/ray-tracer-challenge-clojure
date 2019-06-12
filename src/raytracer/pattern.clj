4df42e2a80bc67b46bc7eba3d94ec12a48bb46f8
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2019-06-12 09:40:06 +0200

    Convert pattern in a way that preserves equality
    Some performances may have been lost due to the function-data accesses,
    it can be probably fixed with more records, should it be a vexing
    slowdown (it's not).

--- src/raytracer/pattern.clj
+++ src/raytracer/pattern.clj
@@ -5,94 +5,92 @@
 (defprotocol ColorFunction
   (color-at [this point] [this object point]))
 
-(defprotocol ObjectSpace
-  (change-transform [this new-transform]))
-
 (defn- point-in-pattern-space [pattern object point]
   (->> point
        (matrix/transform (:inverse-transform object))
        (matrix/transform (:inverse-transform pattern))))
 
-(defrecord Pattern [inverse-transform function]
+(defrecord Pattern [inverse-transform function function-data]
   ColorFunction
   (color-at [this point]
-    (function this point))
+    (function function-data  point))
   (color-at [this object point]
-    (function this (point-in-pattern-space this object point))))
+    (function function-data (point-in-pattern-space this object point))))
 
 (defn- create-pattern
-  ([function]
+  ([function function-data]
      (->Pattern matrix/identity-matrix
-                function))
-  ([function transform]
+                function function-data))
+  ([function function-data transform]
      (->Pattern (matrix/invert transform 4)
-                function)))
+                function function-data)))
 
-(defrecord SolidPattern [color]
-  ColorFunction
-  (color-at [this point]
-    color)
-  (color-at [this object point]
-    color)
-  ObjectSpace
-  (chnage-transform [this new-transform]))
+(defn- solid-color-function [function-data _]
+  (:color function-data))
 
 (defn solid [color]
-  (->SolidPattern color))
+  (create-pattern solid-color-function
+                  {:color color}))
 
-(defn- stripe-function [stripe-pattern point]
+(defn- stripe-color-function [function-data point]
   (if (zero? (mod (int (Math/floor (:x point))) 2))
-    (:color1 stripe-pattern)
-    (:color2 stripe-pattern)))
-
-(defrecord StripePattern [color1 color2 inverse-transform]
-  ColorFunction
-  (color-at [this point]
-    (stripe-function this point))
-  (color-at [this object point]
-    (stripe-function this (point-in-pattern-space this object point)))
-  ObjectSpace
-  (change-transform [this new-transform]
-    ))
+    (:color1 function-data)
+    (:color2 function-data)))
 
 (defn stripe [color1 color2]
-  (->StripePattern color1 color2))
+  (create-pattern stripe-color-function
+                  {:color1 color1, :color2 color2}))
+
+(defn- gradient-color-function [function-data point]
+  (let [x (:x point)
+        p (- x (Math/floor x))]
+    (color/add (color/scale (:color1 function-data) (- 1 x))
+               (color/scale (:color2 function-data) x))))
 
 (defn gradient [color1 color2]
-  (create-pattern (fn [_ point]
-                    (let [x (:x point)
-                          p (- x (Math/floor x))]
-                      (color/add (color/scale color1 (- 1 x))
-                                 (color/scale color2 x))))))
+  (create-pattern gradient-color-function
+                  {:color1 color1, :color2 color2}))
+
+(defn- test-color-function [_ point]
+  (color/color (:x point)
+               (:y point)
+               (:z point)))
 
 (defn test []
-  (create-pattern (fn [_ point]
-                    (color/color (:x point)
-                                 (:y point)
-                                 (:z point)))))
+  (create-pattern test-color-function nil))
+
+(defn- ring-color-function [function-data point]
+  (let [x (:x point)
+        z (:z point)
+        distance (Math/sqrt (+ (* x x) (* z z)))]
+    (if (zero? (mod (int (Math/floor distance)) 2))
+      (:color1 function-data)
+      (:color2 function-data))))
 
 (defn ring [color1 color2]
-  (create-pattern (fn [_ point]
-                    (let [x (:x point)
-                          z (:z point)
-                          distance (Math/sqrt (+ (* x x) (* z z)))]
-                      (if (zero? (mod (int (Math/floor distance)) 2))
-                        color1
-                        color2)))))
+  (create-pattern ring-color-function
+                  {:color1 color1, :color2 color2}))
+
+(defn- checker-color-function [function-data point]
+  (if (zero? (mod (+ (int (Math/floor (+ 2e9 (:x point))))
+                     (int (Math/floor (+ 2e9 (:y point))))
+                     (int (Math/floor (+ 2e9 (:z point))))) 2))
+    (:color1 function-data)
+    (:color2 function-data)))
 
 (defn checker [white black]
-  (create-pattern (fn [_ point]
-                    (if (zero? (mod (+ (int (Math/floor (+ 2e9 (:x point))))
-                                       (int (Math/floor (+ 2e9 (:y point))))
-                                       (int (Math/floor (+ 2e9 (:z point))))) 2))
-                      white
-                      black))))
+  (create-pattern checker-color-function
+                  {:color1 white, :color2 black}))
+
+(defn blend-color-function [function-data point])
 
 (defn blend [pattern-a pattern-b]
-  (create-pattern (fn [_ point]
-                    (color/scale (color/add (color-at pattern-a point)
-                                            (color-at pattern-b point))
-                                 0.5))))
+  (create-pattern (fn [function-data point]
+                    (color/scale
+                     (color/add (color-at (:pattern-a function-data) point)
+                                (color-at (:pattern-b function-data) point))
+                     0.5))
+                  {:pattern-a pattern-a, :pattern-b pattern-b}))
 
 (defn perturb-pattern [pattern perturbation-f]
   (create-pattern (fn [_ point]
@@ -101,4 +99,5 @@
 
 (defn change-transform [pattern new-transform]
   (create-pattern (:function pattern)
+                  (:function-data pattern)
                   new-transform))
