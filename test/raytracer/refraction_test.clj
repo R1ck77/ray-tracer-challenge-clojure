b2d4d401b8531e9a52e3dd2a8ecdbde773571cd0
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2019-07-19 08:35:57 +0200

    Fix the refraction test

--- test/raytracer/refraction_test.clj
+++ test/raytracer/refraction_test.clj
@@ -5,13 +5,15 @@
             [raytracer.point :as point]
             [raytracer.svector :as svector]
             [raytracer.shapes :as shapes]
+            [raytracer.shapes.group :as group]
             [raytracer.transform :as transform]
             [raytracer.material :as material]
             [raytracer.ray :as ray]            
             [raytracer.refraction :as refraction]
             [raytracer.world :as world]            
             [raytracer.intersection :as intersection]
-            [raytracer.shapes-test :as shapes-test]))
+            [raytracer.shapes-test :as shapes-test]
+            [raytracer.grouping.hierarchy :as hierarchy]))
 
 (deftest test-compute-refractive-indices
     (testing "Finding n1 and n2 at various intersections"
@@ -54,30 +56,36 @@
 (deftest test-schlick
   (testing "The Schlick approximation under total internal reflection"
     (let [shape (shapes-test/glass-sphere)
+          hierarchy (hierarchy/hierarchy (group/group [shape]))
           ray (ray/ray (point/point 0 0 half√2)
                        (svector/svector 0 1 0))
           intersections [(intersection/intersection (- half√2) shape)
                          (intersection/intersection half√2 shape)]
-          intermediate-results (world/prepare-computations ray
+          intermediate-results (world/prepare-computations hierarchy
+                                                           ray
                                                            (second intersections)
                                                            {:n1 1.5, :n2 1})]
       (is (eps= 1 (refraction/schlick intermediate-results)))))
   (testing "The Schlick approximation with a perpendicular viewing angle"
     (let [shape (shapes-test/glass-sphere)
+          hierarchy (hierarchy/hierarchy (group/group [shape]))
           ray (ray/ray (point/point 0 0 0)
                        (svector/svector 0 1 0))
           intersections [(intersection/intersection -1 shape)
                          (intersection/intersection 1 shape)]
-          intermediate-results (world/prepare-computations ray
+          intermediate-results (world/prepare-computations hierarchy
+                                                           ray
                                                            (second intersections)
                                                            {:n1 1.5, :n2 1})]
       (is (eps= 0.04 (refraction/schlick intermediate-results)))))
   (testing "The Schlick approximation with small angle and n2 > n1"
     (let [shape (shapes-test/glass-sphere)
+          hierarchy (hierarchy/hierarchy (group/group [shape]))
           ray (ray/ray (point/point 0 0.99 -2)
                        (svector/svector 0 0 1))
           intersections [(intersection/intersection 1.8589 shape)]
-          intermediate-results (world/prepare-computations ray
+          intermediate-results (world/prepare-computations hierarchy
+                                                           ray
                                                            (first intersections)
                                                            {:n1 1, :n2 1.5})]
       (is (eps= 0.48873 (refraction/schlick intermediate-results))))))
