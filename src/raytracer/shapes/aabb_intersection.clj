1f93e1406f90f4fe713cd03313f65f98a05a7b50
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2019-07-27 20:46:19 +0200

    Fix bug in the AABB intersection

--- src/raytracer/shapes/aabb_intersection.clj
+++ src/raytracer/shapes/aabb_intersection.clj
@@ -26,13 +26,13 @@
 (defn local-intersect-t [[min-point max-point] ray]
   (let [origin (:origin ray)
         direction (:direction ray)]
-    (let [[tminx tmaxx] (check-axis -1 1 (:x origin) (:x direction))
-          [tminy tmaxy] (check-axis -1 1(:y origin) (:y direction))
-          min (Math/max (float  tminx) (float tminy))
+    (let [[tminx tmaxx] (check-axis (:x min-point) (:x max-point) (:x origin) (:x direction))
+          [tminy tmaxy] (check-axis (:y min-point) (:y max-point) (:y origin) (:y direction))
+          min (Math/max (float tminx) (float tminy))
           max (Math/min (float tmaxx) (float tmaxy))]
       (if (> min max)
         []
-        (let [[tminz tmaxz] (check-axis (:z origin) (:z direction))
+        (let [[tminz tmaxz] (check-axis (:z min-point) (:z max-point) (:z origin) (:z direction))
               min (Math/max (float min) (float tminz))
               max (Math/min (float max) (float tmaxz))]
           (if (> min max)
