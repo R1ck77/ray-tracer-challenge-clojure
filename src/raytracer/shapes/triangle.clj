dd8012930cb063898fe73c825c85e67a5b690a0e
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2019-08-10 13:49:23 +0200

    Test compute-normal GREEN

--- src/raytracer/shapes/triangle.clj
+++ src/raytracer/shapes/triangle.clj
@@ -1,9 +1,16 @@
 (ns raytracer.shapes.triangle
   (:require [raytracer.point :as point]
             [raytracer.tuple :as tuple]
-            [raytracer.svector :as svector]))
+            [raytracer.svector :as svector]
+            [raytracer.shapes.shared :as shared]))
 
-(defrecord Triangle [p1 p2 p3 e1 e2 normal])
+(defrecord Triangle [p1 p2 p3 e1 e2 normal]
+  shared/Surface
+  (compute-normal [this point]
+    normal)
+  shared/Intersectable
+  (local-intersect [this ray]
+    (throw (UnsupportedOperationException.))))
 
 (defn triangle [p1 p2 p3]
   (let [edge1 (tuple/sub p2 p1)
--- test/raytracer/shapes/triangle_test.clj
+++ test/raytracer/shapes/triangle_test.clj
@@ -3,6 +3,7 @@
             [raytracer.test-utils :as tu]
             [raytracer.point :as point]
             [raytracer.svector :as svector]
+            [raytracer.shapes.shared :as shared]
             [raytracer.shapes.triangle :as triangle]))
 
 (deftest test-constructor
@@ -19,3 +20,13 @@
       (is (tu/t= (svector/svector 1 -1 0 ) (:e2 triangle))))
     (testing "A triangle has a normal"
       (is (tu/t= (svector/svector 0.0 0.0 -1.0) (:normal triangle))))))
+
+(deftest test-compute-normal
+  (testing "Finding the normal on a triangle"
+    (let [triangle (triangle/triangle (point/point 0 1 0)
+                                      (point/point -1 0 0)
+                                      (point/point 1 0 0))
+          normal (:normal triangle)]
+      (is (tu/t= normal (shared/compute-normal triangle (point/point 0 0.5 0))))
+      (is (tu/t= normal (shared/compute-normal triangle (point/point -0.5 0.75 0))))
+      (is (tu/t= normal (shared/compute-normal triangle (point/point 0.5 0.25 0)))))))
