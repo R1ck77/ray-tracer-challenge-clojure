87d878eed68070a560362721547ead076e50c746
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2019-08-10 16:42:22 +0200

    Minor refactoring
    The triangle intersection still looks pretty ugly

--- src/raytracer/shapes/triangle.clj
+++ src/raytracer/shapes/triangle.clj
@@ -6,24 +6,22 @@
             [raytracer.intersection :as intersection]
             [raytracer.shapes.shared :as shared]))
 
-(defn- local-intersect-triangle [triangle ray]
-  (let [dir-cross-e2 (tuple/cross (:direction ray) (:e2 triangle))
-        det (tuple/dot (:e1 triangle) dir-cross-e2)]
+(defn- local-intersect-triangle [{:keys [p1 e1 e2] :as triangle} {:keys [origin direction] :as ray}]
+  (let [dir-cross-e2 (tuple/cross direction e2)
+        det (tuple/dot e1 dir-cross-e2)]
     (if (< (Math/abs (float det)) const/EPSILON)
       []
-      (let [f (/ det)
-            p1-to-origin (tuple/sub (:origin ray) (:p1 triangle))
-            u (* f (tuple/dot p1-to-origin dir-cross-e2))]
-        (cond)
-        (if (or (< u 0) (> u 1))
-          []
-          (let [origin-cross-e1 (tuple/cross p1-to-origin (:e1 triangle))
-                v (* f (tuple/dot (:direction ray) origin-cross-e1))]
-            (if (or (< v 0) (> (+ v u) 1))
-              []
-              [(intersection/intersection (* f (tuple/dot (:e2 triangle)
-                                                          origin-cross-e1))
-                                          triangle)])))))))
+      (let [f (/ det)]
+        (let [p1-to-origin (tuple/sub origin p1)
+              u (* f (tuple/dot p1-to-origin dir-cross-e2))]
+          (if (or (< u 0) (> u 1))
+            []
+            (let [origin-cross-e1 (tuple/cross p1-to-origin e1)
+                  v (* f (tuple/dot direction origin-cross-e1))]
+              (if (or (< v 0) (> (+ v u) 1))
+                []
+                [(intersection/intersection (* f (tuple/dot e2 origin-cross-e1))
+                                            triangle)]))))))))
 
 (defrecord Triangle [p1 p2 p3 e1 e2 normal]
   shared/Surface
