4316df0e04b455145b38f4804918993fb3cdf632
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2019-08-17 19:30:23 +0200

    WIP to partition the various shapes

--- src/raytracer/shapes/optimizers/bisection_recursive.clj
+++ src/raytracer/shapes/optimizers/bisection_recursive.clj
@@ -1,5 +1,6 @@
 (ns raytracer.shapes.optimizers.bisection-recursive
   (:require [raytracer.shapes.optimizers.optimizer :as optimizer]
+            [raytracer.shapes.optimizers.box :as box]
             [raytracer.const :as const]
             [raytracer.shapes.bounding-box :as bounding-box]
             [raytracer.shapes.group :as group]))
@@ -14,13 +15,36 @@
   [shapes]
   (bounding-box/get-corners (group/group shapes)))
 
+(def partition-shapes)
+
+(defn- groups-to-map
+  "Create a map of groups vs shapes assigned to it"
+  [set]
+  (into {} (map #(vector % []) set)))
+
+(defn- assign-shape [{:keys [sub-boxes, outliers]} shape]
+  (let [boxes (keys sub-boxes)
+        assigned (filter #(box/contains % shape) boxes)]
+    (if (empty? assigned)
+      {:sub-boxes sub-boxes, :outliers (conj outliers shape)}
+      ;; hic sunt leones. Assign the shape to the right box
+      )))
+
+(defn- split-shapes [shapes max-size]
+  (let [main-box (box/box (compute-extremes shapes))
+        sub-boxes (box/bisect main-box)]
+    (reduce assign-shape {:sub-boxes (groups-to-map sub-boxes)
+                          :outliers []})))
+
 
 (defn- partition-shapes
   "Recursively partition each group into at most max-size objects"
   [shapes max-size]
-  (let [[min-point max-point] (compute-extremes shapes)]
-    
-    ))
+  (if (<= (count shapes) max-size)
+    (group/group shapes)
+    (split-shapes shapes max-size))
+  
+  )
 
 (defn is-infinite?
   "Returns true if the extension of a shape is infinite.
