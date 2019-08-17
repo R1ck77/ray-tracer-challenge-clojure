fc1dfe22185a206a69ceb94e9a51756bf4201495
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2019-08-17 11:02:13 +0200

    Add function to sort shapes in two vectors GREEN

--- test/raytracer/shapes/optimizers/bisection_recursive_test.clj
+++ test/raytracer/shapes/optimizers/bisection_recursive_test.clj
@@ -16,3 +16,35 @@
     (is (not (br/is-infinite? (shapes/sphere))))
     (is (br/is-infinite? (shared/transform (shapes/sphere)
                                            (transform/scale Double/POSITIVE_INFINITY 0 0))))))
+
+(deftest test-sort-infinite-shapes
+  (let [sort-infinite-shapes #'raytracer.shapes.optimizers.bisection-recursive/sort-infinite-shapes]
+    (testing "Only infinite shapes"
+      (is (= {:finite []
+              :infinite [(shapes/cylinder :maximum 100)
+                         (shapes/cylinder :maximum 99)
+                         (shapes/cylinder :maximum 98)]}
+             (sort-infinite-shapes [(shapes/cylinder :maximum 100)
+                                    (shapes/cylinder :maximum 99)
+                                    (shapes/cylinder :maximum 98)]))))
+    (testing "Only finite shapes"
+      (is (= {:finite [(shapes/sphere)
+                       (shapes/cube)
+                       (shapes/cylinder :minimum 3 :maximum 100)]
+              :infinite []}
+             (sort-infinite-shapes [(shapes/sphere)
+                                    (shapes/cube)
+                                    (shapes/cylinder :minimum 3 :maximum 100)]))))
+    (testing "Mixed shapes"
+      (is (= {:finite [(shapes/sphere)
+                       (shapes/cube)
+                       (shapes/cylinder :minimum 3 :maximum 100)]
+              :infinite [(shapes/cylinder :maximum 100)
+                         (shapes/cylinder :maximum 99)
+                         (shapes/cylinder :maximum 98)]}
+             (sort-infinite-shapes [(shapes/sphere)
+                                    (shapes/cylinder :maximum 100)
+                                    (shapes/cube)
+                                    (shapes/cylinder :minimum 3 :maximum 100)
+                                    (shapes/cylinder :maximum 99)
+                                    (shapes/cylinder :maximum 98)]))))))
