714f998d3ea99cfab2f8d066496763d4934a49e7
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2019-08-20 18:32:31 +0200

    Add UV into the triangle intersection

--- test/raytracer/shapes/smooth_triangle_test.clj
+++ test/raytracer/shapes/smooth_triangle_test.clj
@@ -3,20 +3,35 @@
             [raytracer.test-utils :as tu]
             [raytracer.point :as point]
             [raytracer.svector :as svector]
+            [raytracer.ray :as ray]
+            [raytracer.intersection :as intersection]
+            [raytracer.shapes.shared :as shared]
             [raytracer.shapes.smooth-triangle :as st]))
 
-(def tri (st/smooth-triangle (point/point -1 0 0)
-                             (point/point 1 0 0)
-                             (point/point 0 1 0)
-                             (svector/svector 0 1 0)
-                             (svector/svector -1 0 0)
-                             (svector/svector 1 0 0)))
-
 (deftest test-smooth-triangle
-  (testing "Constructing a smooth triangle"
-    (is (tu/t= (point/point -1 0 0) (:p1 tri)))
-    (is (tu/t= (point/point 1 0 0) (:p2 tri)))
-    (is (tu/t= (point/point 0 1 0) (:p3 tri)))
-    (is (tu/t= (svector/svector 0 1 0) (:n1 tri)))
-    (is (tu/t= (svector/svector -1 0 0) (:n2 tri)))
-    (is (tu/t= (svector/svector 1 0 0) (:n3 tri)))))
+  (let [tri (st/smooth-triangle (point/point -1 0 0)
+                                (point/point 1 0 0)
+                                (point/point 0 1 0)
+                                (svector/svector 0 1 0)
+                                (svector/svector -1 0 0)
+                                (svector/svector 1 0 0))]
+    (testing "Constructing a smooth triangle"
+      (is (tu/t= (point/point -1 0 0) (:p1 tri)))
+      (is (tu/t= (point/point 1 0 0) (:p2 tri)))
+      (is (tu/t= (point/point 0 1 0) (:p3 tri)))
+      (is (tu/t= (svector/svector 0 1 0) (:n1 tri)))
+      (is (tu/t= (svector/svector -1 0 0) (:n2 tri)))
+      (is (tu/t= (svector/svector 1 0 0) (:n3 tri))))))
+
+(deftest test-local-intersect
+  (testing "An intersection with a smooth trinagle stores u/v"
+    (let [tri (st/smooth-triangle (point/point 0 1 0)
+                                  (point/point -1 0 0)
+                                  (point/point 1 0 0)
+                                  (svector/svector 0 1 0)
+                                  (svector/svector -1 0 0)
+                                  (svector/svector 1 0 0))
+          ray (ray/ray (point/point -0.2 0.3 -2)
+                       (svector/svector 0 0 1))]
+      (is (tu/v= [0.45 0.25]
+                 (intersection/getUV (first (shared/local-intersect tri ray))))))))
