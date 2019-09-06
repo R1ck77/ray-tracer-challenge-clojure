e33aa90d1759f22d5fd76e71f7b7e12aeea119f5
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2019-09-06 08:58:22 +0200

    CSG filter-intersections GREEN

--- src/raytracer/shapes/csg.clj
+++ src/raytracer/shapes/csg.clj
@@ -6,30 +6,72 @@
   (is-intersection-allowed? [this left-shape-hit inside-left-shape inside-right-shape])
   (filter-intersections [this xintersection]))
 
-(defrecord CSGUnion []
+(defn- create-intersection-data [intersection is-left-object inside-left inside-right]
+  (list intersection
+        is-left-object
+        inside-left
+        inside-right))
+
+(defn- update-status [state intersection is-left-object]
+  (update state
+          (if is-left-object
+            :inside-left
+            :inside-right)
+          #(not %)))
+
+(defn- categorize-intersection [state intersection left-object intersection-object]
+  (let [is-left-object (shared/includes? left-object intersection-object)]
+    (update (update-status state intersection is-left-object)
+            :intersections-data
+            (fn [intersections]
+              (conj intersections (create-intersection-data intersection
+                                                            is-left-object
+                                                            (:inside-left state)
+                                                            (:inside-right state)))))))
+
+(defn- categorize-intersections [intersections left-object right-object]
+  (:intersections-data
+   (reduce #(categorize-intersection % %2 left-object (:object %2))
+           {:inside-left false
+            :inside-right false
+            :intersections-data []}
+           intersections)))
+
+(defn- discard-intersections [this intersections]
+  (map first
+       (filter (fn [[_ is-left-object inside-left inside-right]]
+                 (is-intersection-allowed? this is-left-object inside-left inside-right))
+               (categorize-intersections intersections (:left-shape this) (:right-shape this)))))
+
+(defrecord CSGUnion [left-shape right-shape]
   CSG
   (is-intersection-allowed? [this left-shape-hit inside-left-shape inside-right-shape]
     (or (and left-shape-hit (not inside-right-shape))
-        (and (not left-shape-hit) (not inside-left-shape)))))
+        (and (not left-shape-hit) (not inside-left-shape))))
+  (filter-intersections [this intersections]
+    (discard-intersections this intersections)))
 
-(defn union []
-  (->CSGUnion))
+(defn union [left-shape right-shape]
+  (->CSGUnion left-shape right-shape))
 
-(defrecord CSGIntersection []
+(defrecord CSGIntersection [left-shape right-shape]
   CSG
   (is-intersection-allowed? [this left-shape-hit inside-left-shape inside-right-shape]
     (or (and left-shape-hit inside-right-shape)
-        (and (not left-shape-hit) inside-left-shape))))
+        (and (not left-shape-hit) inside-left-shape)))
+  (filter-intersections [this intersections]
+    (discard-intersections this intersections)))
 
-(defn intersection []
-  (->CSGIntersection))
+(defn intersection [left-shape right-shape]
+  (->CSGIntersection left-shape right-shape))
 
-(defrecord CSGDifference []
+(defrecord CSGDifference [left-shape right-shape]
   CSG
   (is-intersection-allowed? [this left-shape-hit inside-left-shape inside-right-shape]
     (or (and left-shape-hit (not inside-right-shape))
-        (and (not left-shape-hit) inside-left-shape))))
+        (and (not left-shape-hit) inside-left-shape)))
+  (filter-intersections [this intersections]
+    (discard-intersections this intersections)))
 
-(defn difference []
-  (->CSGDifference))
-
+(defn difference [left-shape right-shape]
+  (->CSGDifference left-shape right-shape))
--- test/raytracer/shapes/csg_test.clj
+++ test/raytracer/shapes/csg_test.clj
@@ -1,12 +1,14 @@
 (ns raytracer.shapes.csg-test
   (:require [clojure.test :refer :all]
+            [raytracer.shapes :as shapes]            
             [raytracer.test-utils :as tu]
+            [raytracer.intersection :as intersection]
             [raytracer.shapes.csg :as csg]))
 
 
 (deftest test-union-intersection-allowed?
   (testing "Rules for the CSG union operation"
-    (let [union (csg/union)]
+    (let [union (csg/union nil nil)]
       (is (not (csg/is-intersection-allowed? union true true true)))
       (is (csg/is-intersection-allowed? union true true false))
       (is (not (csg/is-intersection-allowed? union true false true)))
@@ -16,7 +18,7 @@
       (is (csg/is-intersection-allowed? union false false true))
       (is (csg/is-intersection-allowed? union false false false))))
   (testing "Rules for the CSG intersection operation"
-    (let [intersection (csg/intersection)]
+    (let [intersection (csg/intersection nil nil)]
       (is (csg/is-intersection-allowed? intersection true true true))
       (not (csg/is-intersection-allowed? intersection true true false))
       (is (csg/is-intersection-allowed? intersection true false true))
@@ -26,7 +28,7 @@
       (not (csg/is-intersection-allowed? intersection false false true))
       (not (csg/is-intersection-allowed? intersection false false false))))
   (testing "Rules for the CSG difference operation"
-    (let [difference (csg/difference)]
+    (let [difference (csg/difference nil nil)]
       (is (not (csg/is-intersection-allowed? difference true true true)))
       (is (csg/is-intersection-allowed? difference true true false))
       (is (not (csg/is-intersection-allowed? difference true false true)))
@@ -35,3 +37,19 @@
       (is (csg/is-intersection-allowed? difference false true false))
       (is (not (csg/is-intersection-allowed? difference false false true)))
       (is (not (csg/is-intersection-allowed? difference false false false))))))
+
+(deftest test-filter-intersections
+  (let [sphere (shapes/sphere)
+        cube (shapes/cube)
+        int-0 (intersection/intersection 1 sphere)
+        int-1 (intersection/intersection 2 cube)
+        int-2 (intersection/intersection 3 sphere)
+        int-3 (intersection/intersection 4 cube)
+        intersections [int-0 int-1 int-2 int-3]]
+    (testing "Filtering a list of intersections"
+      (is (= [int-0 int-3]
+             (csg/filter-intersections (csg/union sphere cube) intersections)))
+      (is (= [int-1 int-2]
+             (csg/filter-intersections (csg/intersection sphere cube) intersections)))
+      (is (= [int-0 int-1]
+             (csg/filter-intersections (csg/difference sphere cube) intersections))))))
