933d2fe0a3857f872a39d9f946eafa3fb4c1f123
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2019-09-08 10:17:21 +0200

    Improve CSG demo (!work)

--- test/demo/csg_demo.clj
+++ test/demo/csg_demo.clj
@@ -3,13 +3,24 @@
             [raytracer.svector :as svector]
             [raytracer.world :as world]
             [raytracer.camera :as camera]
-            [raytracer.canvas :as canvas]))
+            [raytracer.canvas :as canvas]
+            [raytracer.shapes :as shapes]
+            [raytracer.transform :as transform]
+            [raytracer.shapes.shared :as shared]
+            [raytracer.shapes.placement :as placement]))
 
 (def ^:dynamic *output-file* "csg-demo.ppm")
 (def ^:dynamic *image-resolution* [800 600])
 
+(defn- create-floor []
+  (shapes/csg :intersection
+              (shared/change-transform (shapes/sphere)
+                                       (transform/scale 100 100 100))
+              (shared/change-transform (shapes/cube)
+                                       (transform/scale 100 0.01 100))))
+
 (defn- create-world []
-  (world/world))
+  (world/world [(create-floor)]))
 
 (defn- create-camera [world width height]
   (camera/camera width height (/ Math/PI 3)))
