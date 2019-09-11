b716e2bf9010ee68afd1183b8da79a2a67d8436f
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2019-09-11 09:10:35 +0200

    Refactoring on a test

--- test/raytracer/world_test.clj
+++ test/raytracer/world_test.clj
@@ -332,34 +332,41 @@
       (is (ta/eps= 0.0 (-> (world/select-shadow-attenuation (create-shadow-test-world [0 25 0])
                                                          (point/point 0 0 0)) first second)))
       (is (ta/eps= 0.0 (-> (world/select-shadow-attenuation (create-shadow-test-world [0 35 0])
-                                                         (point/point 0 0 0)) first second))))))
+                                                            (point/point 0 0 0)) first second))))))
+
+(defn- get-first-attenuation [world point]
+  (-> (world/select-shadow-attenuation world point)
+      first
+      second))
+
 (deftest test-select-shadow-attenuation
   (with-redefs [world/*basic-shade-detection* false]
     (testing "CUSTOM There is no shadow when nothing is collinear with point and light"
-      (is (ta/eps= 1 (-> (world/select-shadow-attenuation default-world (point/point 0 10 0)) first second))))
+      (is (ta/eps= 1
+                   (get-first-attenuation default-world
+                                          (point/point 0 10 0)))))
     (testing "CUSTOM The shadow when an object is between the point and the light"
-      (is (ta/eps= 0 (-> (world/select-shadow-attenuation default-world (point/point 10 -10 10)) first second))))
+      (is (ta/eps= 0 (get-first-attenuation default-world
+                                            (point/point 10 -10 10)))))
     (testing "CUSTOM There is no shadow when an object is behind the light"
-      (is (ta/eps= 1 (-> (world/select-shadow-attenuation default-world (point/point -20 20 -20)) first second))))
+      (is (ta/eps= 1 (get-first-attenuation default-world
+                                            (point/point -20 20 -20)))))
     (testing "CUSTOM There is no shadow when an object is behind the point"
-      (is (ta/eps= 1 (-> (world/select-shadow-attenuation default-world (point/point -2 2 -2)) first second))))
+      (is (ta/eps= 1 (get-first-attenuation default-world
+                                            (point/point -2 2 -2)))))
     (testing "CUSTOM objects between the light and the point are attenuated by the transparency"
-      (is (ta/eps= 1.0 (-> (world/select-shadow-attenuation (create-shadow-test-world [0 5 0])
-                                                            (point/point 0 0.0001 0))
-                           first
-                           second)))
-      (is (ta/eps= 0.2 (-> (world/select-shadow-attenuation (create-shadow-test-world [0 15 0])
-                                                            (point/point 0 0 0))
-                           first
-                           second)))
-      (is (ta/eps= 0.06 (-> (world/select-shadow-attenuation (create-shadow-test-world [0 25 0])
-                                                             (point/point 0 0 0))
-                            first
-                            second)))
-      (is (ta/eps= 0.024 (-> (world/select-shadow-attenuation (create-shadow-test-world [0 35 0])
-                                                              (point/point 0 0 0))
-                             first
-                             second))))))
+      (is (ta/eps= 1.0
+                   (get-first-attenuation (create-shadow-test-world [0 5 0])
+                                          (point/point 0 0.0001 0))))
+      (is (ta/eps= 0.2
+                   (get-first-attenuation (create-shadow-test-world [0 15 0])
+                                          (point/point 0 0 0))))
+      (is (ta/eps= 0.06
+                   (get-first-attenuation (create-shadow-test-world [0 25 0])
+                                          (point/point 0 0 0))))
+      (is (ta/eps= 0.024
+                   (get-first-attenuation (create-shadow-test-world [0 35 0])
+                                          (point/point 0 0 0)))))))
 
 (defn- update-ambient [shape]
   (sshared/change-material shape
