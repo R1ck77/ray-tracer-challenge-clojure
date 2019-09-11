a72273b7e07f5bb230b17c38a18cb5ab8c8637b9
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2019-09-11 08:50:08 +0200

    csg_demo.clj exposes a bug in the rendering
    CSG demo completely transparent unless another shape is added to the
    scene, and the result is still not the best

--- test/demo/csg_demo.clj
+++ test/demo/csg_demo.clj
@@ -1,33 +1,49 @@
 (ns demo.csg-demo
   (:require [raytracer.point :as point]
             [raytracer.svector :as svector]
+            [raytracer.color :as color]
             [raytracer.world :as world]
             [raytracer.camera :as camera]
             [raytracer.canvas :as canvas]
             [raytracer.shapes :as shapes]
             [raytracer.transform :as transform]
+            [raytracer.light-sources :as light-sources]
             [raytracer.shapes.shared :as shared]
             [raytracer.shapes.placement :as placement]))
 
 (def ^:dynamic *output-file* "csg-demo.ppm")
 (def ^:dynamic *image-resolution* [800 600])
 
+(def platform-size 20)
+
 (defn- create-floor []
-  (shapes/csg :intersection
+  #_(shapes/csg :intersection
               (shared/change-transform (shapes/sphere)
-                                       (transform/scale 100 100 100))
+                                       (transform/scale platform-size
+                                                        platform-size
+                                                        platform-size))
               (shared/change-transform (shapes/cube)
-                                       (transform/scale 100 0.01 100))))
+                                       (transform/scale platform-size
+                                                        0.01
+                                                        platform-size)))
+  (shapes/csg :difference
+              (shapes/cube)
+              (shapes/sphere)))
 
 (defn- create-world []
-  (world/world [(create-floor)]))
+  (world/set-light-sources (world/world [(shapes/sphere) (create-floor)])
+                           (light-sources/create-point-light (point/point 0 10 -10)
+                                                             (color/color 1 1 1))))
 
-(defn- create-camera [world width height]
-  (camera/camera width height (/ Math/PI 3)))
+(defn- create-camera [width height]
+  (camera/set-transform (camera/camera width height (/ Math/PI 3))
+                        (world/view-transform (point/point 0 3 -5)
+                                              (point/point 0 1 0)
+                                              (svector/svector 0 1 0))))
 
 (defn- render-world [width height]
   (let [world (create-world)]
-    (camera/render (create-camera world width height)
+    (camera/render (create-camera width height)
                    world)))
 
 (defn render-demo
