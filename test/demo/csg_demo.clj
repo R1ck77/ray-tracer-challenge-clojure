43129d687ce0704be9eea83b75a31383f10237f8
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2019-09-12 09:01:56 +0200

    WIP on the CSG demo

--- test/demo/csg_demo.clj
+++ test/demo/csg_demo.clj
@@ -1,10 +1,13 @@
 (ns demo.csg-demo
   (:require [raytracer.point :as point]
+            [raytracer.const :as const]
             [raytracer.svector :as svector]
             [raytracer.color :as color]
             [raytracer.world :as world]
             [raytracer.camera :as camera]
             [raytracer.canvas :as canvas]
+            [raytracer.material :as material]
+            [raytracer.pattern :as pattern]
             [raytracer.shapes :as shapes]
             [raytracer.transform :as transform]
             [raytracer.light-sources :as light-sources]
@@ -14,24 +17,55 @@
 (def ^:dynamic *output-file* "csg-demo.ppm")
 (def ^:dynamic *image-resolution* [800 600])
 
-(def platform-size 20)
+(def platform-size 5)
+
+(defn create-wall-material [color-a color-b]
+  (material/material :specular 0.3
+                     :reflectivity 0.01
+                     :transparency 0.0
+                     :ambient 0.1
+                     :diffuse 0.6
+                     :pattern (pattern/change-transform (pattern/checker color-a color-b)
+                                                        (transform/scale 0.25 0.25 0.25))))
+
+
+(def walls-material (create-wall-material (color/color 0 0.8 0)
+                                          (color/color 1 1 1)))
+
+(defn- create-wall []
+  (-> (shapes/plane)
+      (shared/change-transform (transform/translate 0 0 5 (transform/rotate-x const/half𝛑)))
+      (shared/change-material walls-material)))
+
+(def floor-material (create-wall-material (color/color 0.8 0 0)
+                                          (color/color 1 1 1)))
 
 (defn- create-floor []
-  #_(shapes/csg :intersection
-              (shared/change-transform (shapes/sphere)
-                                       (transform/scale platform-size
-                                                        platform-size
-                                                        platform-size))
-              (shared/change-transform (shapes/cube)
-                                       (transform/scale platform-size
-                                                        0.01
-                                                        platform-size)))
-  (shapes/csg :difference
-              (shapes/cube)
-              (shapes/sphere)))
+  (-> (shapes/plane)
+      (shared/change-transform (transform/rotate-x 0))
+      (shared/change-material floor-material)))
+
+
+(def lens-material (material/material :specular 0.8
+                                      :reflectivity 0.001
+                                      :ambient 0.05
+                                      :diffuse 0.05
+                                      :transparency 0.99
+                                      :refractive-index 1.01
+                                      :pattern (pattern/solid (color/color 0.8 0.8 1))))
+
+(defn- create-lens []
+  (shared/change-transform
+   (shapes/csg :intersection
+               (shared/change-material (shapes/sphere)
+                                       lens-material)
+               (shared/change-material (shared/change-transform (shapes/sphere)
+                                                                (transform/translate 0.5 0 0))
+                                       lens-material))
+                           (transform/rotate-y const/half𝛑)))
 
 (defn- create-world []
-  (world/set-light-sources (world/world [(shapes/sphere) (create-floor)])
+  (world/set-light-sources (world/world [(create-floor) (create-wall) (create-lens)])
                            (light-sources/create-point-light (point/point 0 10 -10)
                                                              (color/color 1 1 1))))
 
